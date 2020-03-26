4497a3cc6cdf44f009551cd4c382e26988dc9338
Ryan Parrish <rino3717@gmail.com>
2020-03-26 16:37:15 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 
+def my_collect(array)
+    i = 0
+    new_arr = []
+    while i < array.length
+      new_arr << yield(array[i])
+      i = i + 1
+       
+    end
+    new_arr
+    
+end
+  
 
+ # myCollect(array) do |name| 
+   # name.split(" ").first
